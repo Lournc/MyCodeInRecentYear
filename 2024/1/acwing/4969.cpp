0565bee7e47c5ed8e6e77930befc8dd4e32fec23
koarz <3577087577@qq.com>
2024-01-17 17:41:43 +0800

    fix4969

--- 2024/1/acwing/4969.cpp
+++ 2024/1/acwing/4969.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <stack>
 #include <string>
 using namespace std;
 
 unsigned count(string &str) {
   unsigned ans{};
-  for (int i = 0; i < str.size() - 1; i++) {
-    if (str[i] == str[i + 1]) {
+  for (int i = 0; i < str.size() - 2; i++) {
+    if (str[i] == str[i + 1] || str[i] == '?' || str[i + 1] == '?') {
       ans++;
       i++;
     }
@@ -16,24 +15,7 @@ unsigned count(string &str) {
 
 int main() {
   string str;
-  str.resize(2);
   cin >> str;
-  stack<char> st;
-  st.push(str[0]);
-  int num{};
-  for (char &v : str) {
-    if (v == '?') {
-      v = num & 1 ? st.top() : str[std::min(str.find('?') + 1, str.size() - 1)];
-      if (v == '?') {
-        v = str[std::min(str.find('?') + 1, str.size() - 1)] = '0';
-        st.push('0');
-      }
-    } else if (v != st.top()) {
-      st.push(v);
-      num = 0;
-    }
-    num++;
-  }
   cout << count(str);
   return 0;
 }
