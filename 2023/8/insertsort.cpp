39d80cc3892ce44da4a9ea09cfa99d96f4ffa82d
koarz <3577087577@qq.com>
2024-03-07 18:03:41 +0800

    change some sort impl

--- 2023/8/insertsort.cpp
+++ 2023/8/insertsort.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
-int main()
-{
-    int a[8] = {70, 50, 30, 20, 10, 70, 40, 60};
-    for (int i = 1; i < 8; i++)
-    {
-        int b = i;
-        for (int j = i - 1; j >= 0; j--)
-        {
-            if (a[b] < a[j])
-                std::swap(a[b--], a[j]);
-        }
+int main() {
+  int a[] = {9, 5, 5, 4, 12, 32, 2, 78, 65};
+  for (int i = 1; i < std::size(a); i++) {
+    int b = i;
+    for (int j = i - 1; j >= 0; j--) {
+      if (a[b] < a[j]) {
+        std::swap(a[b--], a[j]);
+      }
     }
-    for (int i = 0; i < sizeof(a) / sizeof(int); i++)
-    {
-        std::cout << a[i] << ' ';
-    }
-    return 0;
+  }
+  for (int i = 0; i < std::size(a); i++) {
+    std::cout << a[i] << ' ';
+  }
+  return 0;
 }
--- 2023/8/selectsort.cpp
+++ 2023/8/selectsort.cpp
@@ -1,20 +1,14 @@
+#include <algorithm>
 #include <iostream>
+#include <iterator>
 #include <utility>
-int main()
-{
-    int arr[]{5, 8, 1, 4, 6, 5, 7, 1, 33, 4};
-    for (int i = 0; i < sizeof(arr) / sizeof(int) - 1; i++)
-    {
-        int min{i};
-        for (int j = i; j < sizeof(arr) / sizeof(int); j++)
-        {
-            min = arr[min] > arr[j] ? j : min;
-        }
-        std::swap(arr[min], arr[i]);
-    }
-    for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
-    {
-        std::cout << arr[i] << ' ';
-    }
-    return 0;
+int main() {
+  int arr[]{12, 6, 84, 45, 12, 36, 98, 74, 10, 11, 1, 3};
+  for (int i = 0; i < std::size(arr) - 1; i++) {
+    std::swap(*std::min_element(&arr[i], std::end(arr)), arr[i]);
+  }
+  for (int i = 0; i < std::size(arr); i++) {
+    std::cout << arr[i] << ' ';
+  }
+  return 0;
 }
