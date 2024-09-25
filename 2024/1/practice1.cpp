f0f7a2b2645c624ed73303d4651ddfc0ed08b754
koarz <3577087577@qq.com>
2024-09-25 21:14:30 +0800

    compleate filter_view

--- 2024/1/practice1.cpp
+++ 2024/1/practice1.cpp
@@ -7,14 +7,31 @@ template <typename Iter, typename Func> class filter_view {
   Iter begin_;
   Iter end_;
   Func func_;
-  class Iterator {
-
-  };
 
 public:
   filter_view(Iter begin, Iter end, Func func)
       : begin_(begin), end_(end), func_(func) {}
-      
+  struct Iterator {
+    Iter ite_;
+    Iter end_;
+    Func func_;
+    Iterator &operator++() {
+      ite_++;
+      if (ite_ != end_)
+        while (this->func_(*ite_))
+          ite_++;
+      return *this;
+    }
+
+    int operator*() { return *ite_; }
+
+    friend bool operator==(Iterator &l, Iterator &r) {
+      return l.ite_ == r.ite_ && l.func_ == r.func_;
+    }
+  };
+
+  Iterator begin() { return {begin_, end_, func_}; }
+  Iterator end() { return {end_, end_, func_}; }
 };
 int main() {
   vector v{1, 2, 3, 4, 5};
