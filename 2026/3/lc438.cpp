acbd1fc61ee6409031e5afe9fb73bea214438dda
koarz <3577087577@qq.com>
2026-08-29 21:09:10 +0800

    2026/3

---
+++ 2026/3/lc438.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> findAnagrams(string s, string p) {
+  vector<int> res;
+
+  return res;
+}
+
+int main() {
+  string s, p;
+  for (int v : findAnagrams(s, p)) {
+    cout << v;
+  }
+  return 0;
+}
